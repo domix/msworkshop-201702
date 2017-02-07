544bce37d618059498caf211fe3a3ee08f06d5ba
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-02-06 22:16:30 -0500

    init

---
+++ src/main/java/com/cacti/workshop/model/Person.java
@@ -0,0 +1,26 @@
+package com.cacti.workshop.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by domix on 2/3/17.
+ */
+@Getter
+@Setter
+public class Person {
+  private String id;
+  private String name;
+  private String email;
+
+
+  public static Person from(String id, String name, String email) {
+    Person person = new Person();
+    person.setEmail(email);
+    person.setName(name);
+    person.setId(id);
+    return person;
+  }
+}
---
+++ src/test/java/com/cacti/workshop/web/PersonsControllerTests.java
@@ -0,0 +1,26 @@
+package com.cacti.workshop.web;
+
+import com.cacti.workshop.business.NotFoundException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by domix on 2/3/17.
+ */
+public class PersonsControllerTests {
+
+  @Test
+  public void shouldFindAPersonById() throws Throwable {
+    PersonsController personsController = new PersonsController();
+    personsController.init();
+    assertNotNull(personsController.findById("domix"));
+  }
+
+  @Test(expected = NotFoundException.class)
+  public void shouldFailDueNotFoundPerson() throws Throwable {
+    PersonsController personsController = new PersonsController();
+    personsController.init();
+    personsController.findById("domssix");
+  }
+}
